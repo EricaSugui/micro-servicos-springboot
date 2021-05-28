db32887ba6ad348e32047b99a513f681221c36f5
EricaSugui <ericasugui@gmail.com>
2021-05-27 23:29:01 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Bob', 200);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Maria', 300);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Alex', 250);
